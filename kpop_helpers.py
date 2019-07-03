468f295bb71ae282ef476d961d7366ba2c7c633e
yampritelpa <49921625+yampritelpa@users.noreply.github.com>
2019-07-03 09:52:46 -0700

    Update kpop_helpers.py

--- kpop_helpers.py
+++ kpop_helpers.py
@@ -1,88 +1,103 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 25 17:29:13 2019
+Created on Tue Jul  2 23:28:21 2019
 
-The motivation for this is to use data to identify my next top 3 in Kpop.
-Background: I got into Kpop through Brown Eyed Girls, miss A, and Secret. 
-These groups do not perform anymore. Since then, my new top 3 has become
-Red Velvet, EXID, and Mamamoo. Now, these 3 are nearing the end of their
-career high. Could G-IDLE be next? Or is there some dark horse that I
-have not considered.
-I want to use musical analysis to drive the comparison. I will use the
-Spotify API to get this data. For each track, I can access audio features,
-metadata for the whole track, as well as audio analysis throughout the
-audio sequence.
+This file contains helper functions that connect to Spotify's API through the
+spotipy module and then extract data. It loads all data for all tracks with the
+artist, including compilations, rereleases, and guest performances. Spotify has
+a 50-album limit per artist, which is reasonable. 
 
-
-Then, I can build a recommendation tool
-that will suggest me 3 new groups. 
+I tried to edit out all the for loops, but it's still quite slow. 
 
 
 @author: Priyam
 """
+
+
+client_id = '7cf30fc394274566bd5766cb05d66e89'
+client_secret = '9d5ffc176b494fdebd24c970832358dd'
+
+album_limit = 50
+import spotipy
+
+from spotipy.oauth2 import SpotifyClientCredentials
+from kpop_playlist_v2 import twice, blackpink
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+"""
+From each track: name, features
+From each album: title, release date, tracklist, track (also available markets)
+From each artist: name, followers, popularity, albums
+"""
 import pandas as pd
-import requests
-from kpop_config import TOKEN
-
-TOKEN = 'Bearer '+ TOKEN
-
-headers={'Authorization': TOKEN}
-
-def extractCode(url):
-    c = url.split('/')[-1]
-    return c
+import numpy as np
 
 
-def getAnalysis(code):
-    a = 'https://api.spotify.com/v1/audio-analysis/' + code
-    response = requests.get(a,headers=headers)
-    js = response.json()
-    return js
 
-def url2get(url):
-    c = extractCode(url)
-    return getAnalysis(c)
-
-def AnalfromDB(db,song):
-    url = db[db['song title']==song]['analysis_url'].values[0]
-    c = extractCode(url)
-    js = getAnalysis(c)
-    return(js)
-
-def getManyFeats(list_of_song_nicknames):
-    s = 'https://api.spotify.com/v1/audio-features?ids='
-    for n in list_of_song_nicknames:
-        c = extractCode(n)
-        if n==1:
-            s = s + c
-        else:
-            s = s + ',' + c
-    return s
-
-def getFeats(s):
-    response = requests.get(s,headers = headers)
-    js=response.json()
-    return pd.DataFrame(js['audio_features'][1:])
+def add_track(track_no):#in tracklist
+        track_id = track_no['id']
+        track_feats = sp.audio_features(track_id)[0]
+        title = track_no['name']
+        try:
+            track_feats.update({'song title':title})
+            return track_feats
+        except:
+            print("no features for " + title)
+            return np.nan
+ 
 
 
-def getFeatsPlaylist(playlist):
-    url_list = list(playlist.values())
-    song_list = pd.DataFrame(playlist.keys())
-    song_list = song_list.rename(columns={0:'song title'})
-    df = getFeats(getManyFeats(url_list))
-    df = df.drop(['id','track_href','type','uri'],1)
-    return pd.concat([song_list,df],1)
+            
+def add_album(album):
+    album_id = album['id']
+    tracklist = sp.album_tracks(album_id)['items']
+    album_dict = (add_track(track_no) for track_no in tracklist)
+    
+    
+
+    album_df = pd.DataFrame(album_dict)
+    album_df['album title'] = album['name']
+    album_df['album group'] = album['album_group']
+    album_df['album type'] = album['album_type']
+    album_df['release date'] = album['release_date']    
+    
+    return (album_df)
+
+def concat_albums(albums):
+    return pd.concat([add_album(album) for album in albums],0)
 
 
-def hist(group):
-    for n in ['acousticness','danceability','duration_ms','energy',
-              'loudness','mode','tempo','time_signature','valence']:
-        plt.hist(group[n])
-        plt.title(n)
-        plt.show()
-        
-def stripFeatsTable(db):
-    db = db.drop('analysis_url',1)
-    titles = db['song title']
-    db = db.drop('song title',1)
-    return titles,db
+def artist_update(artist):
+
+    artist_albums = sp.artist_albums(artist,limit=album_limit)
+    albums = artist_albums['items']
+    
+    
+    artist_library = concat_albums(albums)
+    artist_library['artist name']=sp.artist(artist)['name']
+
+    return artist_library
+
+
+def get_artist_info(artist):
+    a_dict = sp.artist(artist)
+    artist_info = {'artist name':a_dict['name'],
+                  'followers': a_dict['followers']['total'],
+                  'genres':[a_dict['genres']],
+                  'popularity':a_dict['popularity'],
+                  }
+    return artist_info
+
+def artists_music(artists):
+    df = pd.concat([artist_update(artist) for artist in artists],0)
+    df = df.reset_index(drop=True).drop(['analysis_url','id','track_href','type','uri'],1)
+    return df
+    
+def artists_info(artists):
+    artists_info = (get_artist_info(artist) for artist in artists)
+    return pd.DataFrame(artists_info)
+
+
