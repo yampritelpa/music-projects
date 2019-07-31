97b68e81c60c2c95166e09a5ffdfbdafb45660ba
yampritelpa <49921625+yampritelpa@users.noreply.github.com>
2019-07-31 14:46:43 -0700

    Update kpop_helpers.py

--- kpop_helpers.py
+++ kpop_helpers.py
@@ -16,7 +16,7 @@ I tried to edit out all the for loops, but it's still quite slow.
 
 username = '1tu40rrjfobuz7luh9rctvb9g'
 client_id = '7cf30fc394274566bd5766cb05d66e89'
-client_secret = '9d5ffc176b494fdebd24c970832358dd'
+client_secret = XXX
 
 album_limit = 50
 import spotipy
